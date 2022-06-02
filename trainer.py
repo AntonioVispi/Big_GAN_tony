23d8f8b8ea1f172933865bcef6fdb958ea43d252
Antoniooooo <102518682+AntonioVispi@users.noreply.github.com>
2022-06-03 00:11:32 +0200

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -210,7 +210,7 @@ class Trainer(object):
                            os.path.join(self.sample_path, '{}_fake.png'.format(step + 1)))
 
             #if (step+1) % model_save_step==0:
-            if (step+1) == model_save_step:
+            if (step+1)==int(model_save_step):
                 torch.save(self.G.state_dict(),
                            os.path.join(self.model_save_path, '{}_G.pth'.format(step + 1)))
                 torch.save(self.D.state_dict(),
