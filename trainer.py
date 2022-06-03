9ef0eee525b2115a5ef151848d167aa7be4fecca
Antoniooooo <102518682+AntonioVispi@users.noreply.github.com>
2022-06-04 01:25:51 +0200

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -78,7 +78,12 @@ class Trainer(object):
     def label_sampel(self):
         label = torch.LongTensor(self.batch_size, 1).random_()%self.n_class
         one_hot= torch.zeros(self.batch_size, self.n_class).scatter_(1, label, 1)
-        return label.squeeze(1).to(self.device), one_hot.to(self.device)       
+        return label.squeeze(1).to(self.device), one_hot.to(self.device)  
+    
+    def label_sampel_NUOVO(self):
+        label = torch.LongTensor(NUM_IMAGES, 1).random_()%self.n_class
+        one_hot= torch.zeros(NUM_IMAGES, self.n_class).scatter_(1, label, 1)
+        return label.squeeze(1).to(self.device), one_hot.to(self.device) 
 
     def train(self):
 
